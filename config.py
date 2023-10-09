36cb840e24669acc04ad3e79ef5b62746d29d61c
Maria <99672002+mshumilova@users.noreply.github.com>
2023-10-09 03:48:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 config = {}
 
-parent_dir = '/Users/mariashumilova/Documents/bioinf/my_projects/fin_pd/project_compound'
-plink_dir = '/Users/mariashumilova/Documents/bioinf/tools/plink_mac/'
-input_dir = '/Users/mariashumilova/Documents/bioinf/my_projects/fin_pd/data'
-selected_files = None
-genome_assembly = 37 # or 37
+parent_dir = '' #path to the working directory
+plink_dir = '' #path to the plink directory
+input_dir = '' #path to the directory with input .BED files
+selected_files = None #or list [] with names of selected .BED files
+genome_assembly = 38 # or 37
 
 config['parent_dir'] = parent_dir
 config['plink_dir'] = plink_dir
